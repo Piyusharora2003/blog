5c20df0a3423963fa323693f4859cd34c29e22ec
Piyush Arora <74443858+Piyusharora2003@users.noreply.github.com>
2023-05-21 00:31:29 +0530

    new editor working fine

--- app/add/page.js
+++ app/add/page.js
@@ -1,19 +1,30 @@
 'use client'
-import React,{useState} from 'react';
-import ReactQuill from "react-quill"
-import 'react-quill/dist/quill.snow.css'
 
-export default function NewArticle() {
-  const [convertedText, setConvertedText] = useState("Some default content");
-  return (
-    <div>
-        Text Editpr
-      <ReactQuill
-        theme='snow'
-        value={convertedText}
-        onChange={setConvertedText}
-        style={{minHeight: '300px'}}
-      />
-    </div>
-  );
-}
+import React, { useState, useRef, useMemo } from 'react';
+import JoditEditor from 'jodit-react';
+
+export default function NewArticle({ placeholder }){
+	const editor = useRef(null);
+	const [content, setContent] = useState('');
+
+	const config = useMemo(()=>{
+			readonly: false; // all options from https://xdsoft.net/jodit/docs/,
+			placeholder : placeholder || 'Start typings...';
+		},
+		[placeholder]
+	);
+
+	return (
+        <>
+		    <JoditEditor
+		    	ref={editor}
+		    	value={content}
+		    	config={config}
+		    	tabIndex={1} // tabIndex of textarea
+		    	onBlur={newContent => setContent(newContent)} // preferred to use only this option to update the content for performance reasons
+		    	onChange={newContent => {setContent(content)}}
+                />
+        </>
+	);
+};
+
