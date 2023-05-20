5ef1528702c8af8261d7d6b5261c3f4ed1074a6c
Piyush Arora <74443858+Piyusharora2003@users.noreply.github.com>
2023-05-21 01:20:37 +0530

    working

--- app/add/page.js
+++ app/add/page.js
@@ -24,7 +24,8 @@ export default function NewArticle({ placeholder }){
 		    	config={config}
 		    	tabIndex={1} // tabIndex of textarea
 		    	onBlur={newContent => setContent(newContent)} // preferred to use only this option to update the content for performance reasons
-		    	onChange={newContent => console.log(newContent)}
+		    	onChange={newContent => console.log(newContent)
+                }
                 />
         </div>
         </>
