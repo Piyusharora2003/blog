c7f5fc1027d9a4c118b0c74e913d1ddb1865fd69
Piyush Arora <74443858+Piyusharora2003@users.noreply.github.com>
2023-05-21 03:40:39 +0530

    complex editor try#1

--- app/add/page.js
+++ app/add/page.js
@@ -1,21 +1,32 @@
-"use client";
-import dynamic from "next/dynamic";
-import React, { useState } from 'react';
-import 'react-quill/dist/quill.snow.css';
+ 'use client';
+ import dynamic from "next/dynamic";
 
-const ReactQuill = dynamic(() => import('react-quill'), {
-  ssr: false,
-  loading: () => <p>Loading...</p>,
-});
-
-function NewArticle() {
-  const [value, setValue] = useState('');
-
-  return (
-    <div>
-      <ReactQuill theme="snow" value={value} onChange={setValue} />
-    </div>
-  );
-}
-
-export default NewArticle;
+ import React, { useState, useRef, useMemo } from 'react';
+ const JoditEditor = dynamic(() => import('jodit-react'), {
+	ssr: false,
+	loading: () => <p>Loading...</p>,
+  });
+export default function NewArticle(){
+ 	const [content, setContent] = useState('');
+ 	const config = useMemo(()=>{
+ 			readonly: false; // all options from https://xdsoft.net/jodit/docs/,
+ 			placeholder : 'Start typings...';
+ 		},
+ 		[]
+ 	);
+ 	return (
+         <>
+          <div className='w-11/12 mx-auto'>
+         <header className='text-3xl my-4 font-bold'>Write Your Post here </header>
+ 		    <JoditEditor
+ 		    	value={content}
+ 		    	config={config}
+ 		    	tabIndex={1} // tabIndex of textarea
+ 		    	onBlur={newContent => setContent(newContent)} // preferred to use only this option to update the content for performance reasons
+ 		    
+                 />
+         </div>
+         </>
+ 	);
+ };
+ 
