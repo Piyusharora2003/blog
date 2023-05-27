2fa4e740aae5acc895b9886ed4d427910c2b9519
Piyush Arora <74443858+Piyusharora2003@users.noreply.github.com>
2023-05-28 00:57:48 +0530

    minor issue

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import CardHcollection from "@/components/HorizontalCard1/CardHcollection";
 import Nav from "@/components/Navbar/Nav";
 import CardVcollection from "@/components/VerticalCard1/CardVcollection";
 
-async function getServerSideProps() {
+async function getlatestpost() {
  const res = await fetch(`http://localhost:3000/api/getlatestposts`);
  const data = (await res.json()).posts;
 //  console.log(data);
@@ -13,7 +13,7 @@ async function getServerSideProps() {
 }
 
 export default async function Home() {
-    const data = await getServerSideProps();
+    const data = await getlatestpost();
     // console.log("data is new one ");
     // console.log(data.data);
     const finaldata = await data.data;
